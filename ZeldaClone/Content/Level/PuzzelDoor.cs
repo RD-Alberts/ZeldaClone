47da778e2a2ccbf80e025ba2fc07c254719d0282
RD-Alberts <RD.Alberts@outlook.com>
2015-04-02 14:34:36 +0100

    Comimit Code

---
+++ ZeldaClone/Content/Level/PuzzelDoor.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using ZeldaClone.Content.PlayerClass;
+using ZeldaClone.Recources;
+
+namespace ZeldaClone.Content.Level
+{
+    class PuzzelDoor : Sprite
+    {
+        public Switch doorSwitch;
+        public Point room;
+        bool isVisible;
+
+        public PuzzelDoor(Vector2 position, Switch newDoorSwitch, Point newRoom)
+            : base(Assets.switchDoor, position)
+        {
+            doorSwitch = newDoorSwitch;
+            room = newRoom;
+            hitTest = new Rectangle((int)position.X, (int)position.Y, 16, 16);
+            isVisible = true;
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            isVisible = !doorSwitch.pressed;
+            if (!isVisible && Local.collisions.Contains(hitTest))
+                Local.collisions.Remove(hitTest);
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            if (isVisible)
+                base.Draw(spriteBatch);
+        }
+    }
+}
